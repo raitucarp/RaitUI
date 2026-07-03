cbbd2df7bd98ed36a580781c6ca1f2ddb8ec2caf
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2026-07-04 02:19:08 +0700

    chore: initialize project scaffold with module and entry point

---
+++ go.mod
@@ -0,0 +1,22 @@
+module raitui
+
+go 1.26.3
+
+require (
+	github.com/hajimehoshi/ebiten/v2 v2.9.9
+	goda v0.0.0
+	golang.org/x/image v0.38.0
+)
+
+require (
+	github.com/alecthomas/participle/v2 v2.1.4 // indirect
+	github.com/ebitengine/gomobile v0.0.0-20260211053922-3d992dae95d1 // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.11.0-alpha.1 // indirect
+	github.com/jezek/xgb v1.3.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
+	golang.org/x/sys v0.42.0 // indirect
+	golang.org/x/text v0.35.0 // indirect
+)
+
+replace goda => ../goda
---
+++ raitui.go
@@ -0,0 +1,59 @@
+package raitui
+
+import (
+	"raitui/component"
+	"raitui/core"
+	"raitui/theme"
+)
+
+var (
+	Box        = component.Box
+	VStack     = component.VStack
+	HStack     = component.HStack
+	Text       = component.Text
+	Flex       = component.Flex
+	Center     = component.Center
+	Container  = component.Container
+	Separator  = component.Separator
+	Spacer     = component.Spacer
+	Heading    = component.Heading
+	Code       = component.Code
+	Link       = component.Link
+	Blockquote = component.Blockquote
+	List       = component.List
+	Kbd        = component.Kbd
+	Mark        = component.Mark
+	Button      = component.Button
+	OutlineButton = component.OutlineButton
+	Checkbox    = component.Checkbox
+	Switch      = component.Switch
+	Input       = component.Input
+	TextArea    = component.TextArea
+
+	Colors = theme.Colors
+
+	bgColor = theme.Gray50
+)
+
+func App() *core.Element {
+	return VStack().
+		Width("100%").Height("100%")
+}
+
+func SetDefaultBackground(c theme.Color) {
+	bgColor = c
+}
+
+func NewContext() *core.Context {
+	return core.NewContext(bgColor)
+}
+
+func SetState(key string, value any)               { core.SetState(key, value) }
+func GetState(key string) (any, bool)               { return core.GetState(key) }
+func DeleteState(key string)                        { core.DeleteState(key) }
+func SetLocalState(componentID, key string, v any)  { core.SetLocalState(componentID, key, v) }
+func GetLocalState(componentID, key string) (any, bool) { return core.GetLocalState(componentID, key) }
+func StateInt(key string, def int) func() int       { return core.StateInt(key, def) }
+func StateStr(key string, def string) func() string { return core.StateStr(key, def) }
+func StateGetter[T any](key string, def T) func() T { return core.StateGetter(key, def) }
+func Setter[T any](key string) func(T)              { return core.Setter[T](key) }
