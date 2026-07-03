934305e0474d34c0ef4957a8dff69b5ebf459b12
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2026-07-04 02:19:51 +0700

    feat(props): add layout constant re-exports from goda

---
+++ props/props.go
@@ -0,0 +1,57 @@
+package props
+
+import goda "goda"
+
+type Justify = goda.Justify
+type Align = goda.Align
+type FlexDirection = goda.FlexDirection
+type Wrap = goda.Wrap
+type PositionType = goda.PositionType
+type Display = goda.Display
+type Overflow = goda.Overflow
+type BoxSizing = goda.BoxSizing
+type Direction = goda.Direction
+
+var (
+	JustifyFlexStart    = goda.JustifyFlexStart
+	JustifyCenter       = goda.JustifyCenter
+	JustifyFlexEnd      = goda.JustifyFlexEnd
+	JustifySpaceBetween = goda.JustifySpaceBetween
+	JustifySpaceAround  = goda.JustifySpaceAround
+	JustifySpaceEvenly  = goda.JustifySpaceEvenly
+
+	AlignFlexStart    = goda.AlignFlexStart
+	AlignCenter       = goda.AlignCenter
+	AlignFlexEnd      = goda.AlignFlexEnd
+	AlignStretch      = goda.AlignStretch
+	AlignBaseline     = goda.AlignBaseline
+	AlignSpaceBetween = goda.AlignSpaceBetween
+	AlignSpaceAround  = goda.AlignSpaceAround
+	AlignSpaceEvenly  = goda.AlignSpaceEvenly
+
+	FlexDirectionRow            = goda.FlexDirectionRow
+	FlexDirectionColumn         = goda.FlexDirectionColumn
+	FlexDirectionRowReverse     = goda.FlexDirectionRowReverse
+	FlexDirectionColumnReverse  = goda.FlexDirectionColumnReverse
+
+	WrapWrap        = goda.WrapWrap
+	WrapNoWrap      = goda.WrapNoWrap
+	WrapWrapReverse = goda.WrapWrapReverse
+
+	PositionStatic   = goda.PositionTypeStatic
+	PositionRelative = goda.PositionTypeRelative
+	PositionAbsolute = goda.PositionTypeAbsolute
+
+	DisplayFlex = goda.DisplayFlex
+	DisplayNone = goda.DisplayNone
+
+	OverflowVisible = goda.OverflowVisible
+	OverflowHidden  = goda.OverflowHidden
+	OverflowScroll  = goda.OverflowScroll
+
+	BoxSizingBorderBox  = goda.BoxSizingBorderBox
+	BoxSizingContentBox = goda.BoxSizingContentBox
+
+	DirectionLTR = goda.DirectionLTR
+	DirectionRTL = goda.DirectionRTL
+)
